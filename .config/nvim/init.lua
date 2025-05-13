3c23beb464e7a623ab4c9e0f5b5828de3d24979f
Abhisek Roy <abhisek.kai@gmail.com>
2025-05-13 11:24:18 +0530

    Added VimBeGood

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -253,7 +253,10 @@ rtp:prepend(lazypath)
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
-
+	{ -- VimBeGood
+		"ThePrimeagen/vim-be-good",
+		cmd = { "VimBeGood" }, -- Will load only when command VimBeGood is entered
+	},
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
 	-- keys can be used to configure plugin behavior/loading/etc.
