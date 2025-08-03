4c6b21c0c849fda6c0ea0c6fab61a0250dcb32c5
Abhisek Roy <abhisek.kai@gmail.com>
2025-08-03 18:51:58 +0530

    Handled sourcekit-lsp finally!

--- ubuntu/.config/nvim/init.lua
+++ ubuntu/.config/nvim/init.lua
@@ -1011,7 +1011,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
@@ -1038,6 +1038,10 @@ require('lazy').setup({
     },
   },
 })
+
+if vim.fn.executable 'sourcekit-lsp' ~= 0 then
+  vim.lsp.enable 'sourcekit'
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
